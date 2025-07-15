65fc2ec24fe486d108cbac0294574c52e7ff0a07
sai-amroji <23b81a7238@cvr.ac.in>
2025-07-15 19:43:35 +0530

    Something

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {NavigationMenu, NavigationMenuContent, NavigationMenuIndicator} from "@/components/ui/navigation-menu.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,11 @@ function App() {
   return (
     <>
       <div>
+          <NavigationMenu>
+              <NavigationMenuContent>
+
+              </NavigationMenuContent>
+          </NavigationMenu>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
